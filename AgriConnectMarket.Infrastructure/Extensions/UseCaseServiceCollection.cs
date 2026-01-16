2ddaac46ab25fad3de5b662b986ce6eb212331e4
Le Duc An <anldce170717@fpt.edu.vn>
2026-01-16 11:15:43 +0700

    Update

--- AgriConnectMarket.Infrastructure/Extensions/UseCaseServiceCollection.cs
+++ AgriConnectMarket.Infrastructure/Extensions/UseCaseServiceCollection.cs
@@ -29,6 +29,7 @@ namespace AgriConnectMarket.Infrastructure.Extensions
             services.AddScoped<ForgotPasswordService>();
             services.AddScoped<ViolationReportService>();
             services.AddScoped<TransactionService>();
+            services.AddScoped<NotificationService>();
 
             return services;
         }
